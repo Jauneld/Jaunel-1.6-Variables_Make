94daee3451348fd6a9c0f0a4039cb6a64f757e10
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 00:38:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+#Jaunel Deans
+#September 18, 2023
+# Variable Assignment
+
+# Assign your name and your favourite food to 2 separate variables with suitable names.
+name = "Jaunel Deans"
+favFood = "Pasta"
+############# SIMPLE ###################
+# Output the contents of the variables on 2 separate lines
+print("\tSimple Task\n")
+print(name)
+print(favFood)
+print()
+############# MEDIUM ####################
+# Output two sentences (not just the contents of the variables). The first with your name, the second with your favourite food.
+print("\tMedium Task\n")
+print("My name is " + name + ".")
+print("My favorite food is " + favFood + ".")
+print()
+############# COMPLEX ###################
+# Output both pieces of information as part of the same sentence.
+# Make sure that you have spaces and punctuation in the correct places. 
+print("\tComplex Task\n")
+print("Hello. My name is " + name + " and my favorite food is " + favFood + ".")
